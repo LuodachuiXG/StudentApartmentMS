8cc2e3a802aca2a15c58dc87c86008612ba0b317
Loac <admin@loac.cc>
2024-01-27 21:11:36 +0800

    修改表结构

--- sams.sql
+++ sams.sql
@@ -3,24 +3,24 @@ CREATE DATABASE IF NOT EXISTS sams CHARACTER SET utf8mb4;
 # 用户表
 CREATE TABLE IF NOT EXISTS user
 (
-    user_id    INT AUTO_INCREMENT COMMENT '用户 ID',
+    userId    INT AUTO_INCREMENT COMMENT '用户 ID',
     name       VARCHAR(100) NOT NULL COMMENT '姓名',
     id         VARCHAR(20)  NOT NULL COMMENT '工号/学号',
     password   VARCHAR(100) NOT NULL COMMENT '密码',
-    role       VARCHAR(10)  NOT NULL COMMENT '身份: student/admin',
+    role       VARCHAR(10)  NOT NULL COMMENT '身份: STUDENT/ADMIN',
     phone      VARCHAR(20)  NOT NULL COMMENT '手机号',
-    gender     VARCHAR(10)  NOT NULL COMMENT '性别: male/female',
+    gender     VARCHAR(10)  NOT NULL COMMENT '性别: MALE/FEMALE',
     birth      DATE         NOT NULL COMMENT '年龄',
-    last_login DATETIME     NULL,
-    CONSTRAINT user_id PRIMARY KEY (user_id)
+    lastLogin DATETIME     NULL,
+    CONSTRAINT userId PRIMARY KEY (userId)
 ) COMMENT '用户表' COLLATE = utf8mb4_unicode_ci;
 
 # 宿舍表
 CREATE TABLE IF NOT EXISTS dormitory
 (
-    dormitory_id INT AUTO_INCREMENT COMMENT '公寓 ID',
+    dormitoryId INT AUTO_INCREMENT COMMENT '公寓 ID',
     name         VARCHAR(50) NOT NULL COMMENT '公寓名',
-    CONSTRAINT dormitory_id PRIMARY KEY (dormitory_id)
+    CONSTRAINT dormitoryId PRIMARY KEY (dormitoryId)
 ) COMMENT '宿舍表' COLLATE = utf8mb4_unicode_ci;
 
 
@@ -28,15 +28,15 @@ CREATE TABLE IF NOT EXISTS dormitory
 # 留言板表
 CREATE TABLE IF NOT EXISTS message_board
 (
-    message_board_id INT AUTO_INCREMENT COMMENT '留言 ID',
-    dormitory_id     INT          NOT NULL COMMENT '宿舍 ID',
-    user_id          INT          NOT NULL COMMENT '用户 ID',
+    messageBoardId INT AUTO_INCREMENT COMMENT '留言 ID',
+    dormitoryId     INT          NOT NULL COMMENT '宿舍 ID',
+    userId          INT          NOT NULL COMMENT '用户 ID',
     content          VARCHAR(200) NOT NULL COMMENT '留言内容',
-    create_date      DATETIME     NOT NULL COMMENT '发布时间',
-    like_count       INT          NOT NULL COMMENT '点赞数量',
+    createDate      DATETIME     NOT NULL COMMENT '发布时间',
+    likeCount       INT          NOT NULL COMMENT '点赞数量',
     top              BOOLEAN      NULL COMMENT '置顶',
-    CONSTRAINT message_board_id PRIMARY KEY (message_board_id),
-    CONSTRAINT dormitory_id FOREIGN KEY (dormitory_id) REFERENCES dormitory (dormitory_id),
-    CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES user (user_id)
+    CONSTRAINT messageBoardId PRIMARY KEY (messageBoardId),
+    CONSTRAINT dormitoryId FOREIGN KEY (dormitoryId) REFERENCES dormitory (dormitoryId),
+    CONSTRAINT userId FOREIGN KEY (userId) REFERENCES user (userId)
 ) COMMENT '留言板' COLLATE = utf8mb4_unicode_ci;
 
